2e4f5baa46247d7b85ae1e0f742bfd03810afe1d
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-02 01:30:01 +0600

    Add files via upload

---
+++ Ascii_Output_With_NewLine_Input.asm
@@ -0,0 +1,69 @@
+.model small
+.stack 100H
+.data
+
+msg db 82
+msg1 db 65
+msg2 db 70
+msg3 db 0
+msg4 db 3  
+msg5 db ?
+
+.code   
+
+main Proc    
+    
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,2
+    mov dl,msg
+    int 21h
+    
+    mov ah,2
+    mov dl,msg1
+    int 21h
+    
+    mov ah,2
+    mov dl,msg2
+    int 21h   
+    
+    mov ah,2
+    add msg2, 3
+    mov dl,msg2
+    int 21h
+            
+    mov ah,2
+    mov dl,msg3
+    int 21h
+    
+    mov ah,2
+    mov dl,msg4
+    int 21h     
+    
+    ;new line
+    
+    mov ah,1
+    int 21h 
+    mov msg5,al
+    
+     
+    mov ah,2
+    mov dl, 10
+    int 21h   
+    
+    mov dl,13
+    int 21h
+    
+    mov ah,2
+    mov dl, msg5
+    int 21h
+    
+        
+        
+    mov ah,4ch
+    int 21h
+    main endp  
+
+end main
+
