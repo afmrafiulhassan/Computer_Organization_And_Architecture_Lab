e86d16070b9912705d694c6705c0c73082ab379a
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-26 21:07:02 +0600

    Add files via upload

---
+++ Lab_Task_1/2.asm
@@ -0,0 +1,29 @@
+.model small
+.stack 100H
+.data
+.code             
+
+
+msg db "Predefine Input: $"
+
+main proc   
+    mov ax,@data
+    mov ds,ax        
+    
+    mov ah,9
+    lea dx,msg
+    int 21h
+            
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+   
+    
+    mov ah,4ch
+    int 21h
+    main endp 
+end main
